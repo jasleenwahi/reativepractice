6609a163c415ca306564156fb0295e1770b0a674
jasleenwahi <jasleen.wahi@knoldus.com>
2023-05-18 18:20:40 +0530

    reactive example

---
+++ src/main/java/com/knoldus/reactive/Exercise4.java
@@ -0,0 +1,22 @@
+package com.knoldus.reactive;
+
+import java.io.IOException;
+
+public class Exercise4 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumberMono()
+
+        // Print the value from intNumberMono when it emits
+        ReactiveSources.intNumberMono().subscribe(number -> System.out.println(number));
+
+
+        // Get the value from the Mono into an integer variable
+        Integer integer = ReactiveSources.intNumberMono().block();
+        System.out.println(integer);
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+
+}
